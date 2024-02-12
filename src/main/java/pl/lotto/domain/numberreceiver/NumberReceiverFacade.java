3d47b87b2126fbe757ba332cdbdab8fa3758e390
damjoz1990 <damjoz1990@gmail.com>
2024-02-12 06:03:47 +0100

    added clock, added repository and in memory testing

--- src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/lotto/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,4 +1,47 @@
 package pl.lotto.domain.numberreceiver;
 
+import lombok.AllArgsConstructor;
+import pl.lotto.domain.numberreceiver.dto.InputNumberResultDto;
+import pl.lotto.domain.numberreceiver.dto.TicketDto;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import static pl.lotto.domain.numberreceiver.dto.InputNumberResultDto.*;
+
+@AllArgsConstructor
 public class NumberReceiverFacade {
+
+    private final NumberValidator validator;
+    private final NumberReceiverRepository repository;
+    private final Clock cloc;
+
+    public InputNumberResultDto inputNumbers(Set<Integer> numbersFromUser){
+       boolean areAllNumbersInRAnge = validator.areAllNumbersInRange(numbersFromUser);
+        if(areAllNumbersInRAnge) {
+            String ticketId = UUID.randomUUID().toString();
+            LocalDateTime drawDate = LocalDateTime.now(cloc);
+            Ticket savedTicket  = repository.save(new Ticket(ticketId, drawDate, numbersFromUser));
+            return builder()
+                    .drawDate(savedTicket.drawDate())
+                    .ticketId(savedTicket.ticketId())
+                    .numbersFromUser(numbersFromUser)
+                    .message("success")
+                    .build();
+        }
+        return builder()
+                .message("failed")
+                .build();
+    }
+
+    public List<TicketDto> userNumbers(LocalDateTime date){
+        List<Ticket> allTicketsByDrawDate = repository.findAllTicketsByDrawDate(date);
+        return allTicketsByDrawDate
+                .stream()
+                .map(ticket -> TicketMapper.mapFromTicket(ticket))
+                .toList();
+    }
+
 }
---
+++ src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
@@ -0,0 +1,6 @@
+package pl.lotto.domain.numberreceiver;
+
+import java.util.Set;
+
+record Ticket(String ticketId, java.time.LocalDateTime drawDate, Set<Integer> numbersFromUser) {
+}
