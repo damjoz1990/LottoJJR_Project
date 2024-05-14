d2c1292a61ed0612e4f69c9373af81dc4938de5a
damjoz1990 <damjoz1990@gmail.com>
2024-05-14 05:28:53 +0200

    added assertions for response from endpoint, and controller

---
+++ src/main/java/pl/lotto/infrastructure/numberreceiver/controller/InputNumberRestController.java
@@ -0,0 +1,37 @@
+package pl.lotto.infrastructure.numberreceiver.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.log4j.Log4j2;
+import pl.lotto.domain.numberreceiver.NumberReceiverFacade;
+import pl.lotto.domain.numberreceiver.dto.NumberReceiverResponseDto;
+import pl.lotto.domain.numberreceiver.dto.TicketDto;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@RestController
+@Log4j2
+@AllArgsConstructor
+public class InputNumberRestController  {
+
+    private final NumberReceiverFacade numberReceiverFacade;
+    @PostMapping("/inputNumbers")
+    public ResponseEntity<NumberReceiverResponseDto> inputNumbers(@RequestBody InputNumbersRequestDto requestDto){
+        Set<Integer> collect = new HashSet<>(requestDto.inputNumbers());
+        NumberReceiverResponseDto numberReceiverResponseDto = numberReceiverFacade.inputNumbers(collect);
+        return ResponseEntity.ok(numberReceiverResponseDto);
+    }
+
+//    @PostMapping("/inputNumbers")
+//    public ResponseEntity<NumberReceiverResponseDto> inputNumbers(@RequestBody InputNumbersRequestDto requestDto) {
+//        Set<Integer> distinctNumbers = new HashSet<>(requestDto.inputNumbers());
+//        NumberReceiverResponseDto numberReceiverResponseDto = numberReceiverFacade.inputNumbers(distinctNumbers);
+//        return ResponseEntity.ok(numberReceiverResponseDto);
+//    }
+}
